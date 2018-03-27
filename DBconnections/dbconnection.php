0db87b8acbde333bc62ec5f4cb39bc7cf0367a2f
John André Seem <s305366@stud.hioa.no>
2018-03-27 17:16:04 +0200

    Edit, delete and search option in the dbfile

--- DBconnections/dbconnection.php
+++ DBconnections/dbconnection.php
@@ -8,11 +8,13 @@ $errors = array();
 $db = mysqli_connect('localhost', 'root', '', 'personer');
 //check if inputs are correct!
 if (isset($_POST['register'])){
-    $username = mysqli_real_escape_string($db, $_POST['username']);
-    $user_type = mysqli_real_escape_string($db, $_POST['user_type']);
-    $password_first = mysqli_real_escape_string($db, $_POST['password_first']);
-    $confirm_password = mysqli_real_escape_string($db, $_POST['confirm_password']);
+    $username = e($_POST['username']);
+    $name = e($_POST['name']);
+    $user_type = e($_POST['user_type']);
+    $password_first = e($_POST['password_first']);
+    $confirm_password = e($_POST['confirm_password']);
     if (empty($username)) {array_push($errors, "You need a username");}
+    if (empty($name)) {array_push($errors, "write your name");}
     if (empty($user_type)) {array_push($errors, "You have to have a role");}
     if ($password_first != $confirm_password){
         array_push($errors, "the password needs to match");
@@ -24,13 +26,16 @@ if (isset($_POST['register'])){
     if($user){
         if($user['username'] === $username){
             array_push($errors, "That username allready exits");
+            echo "exiting username";
         }
     }
+
+
     //add user and cryptate the password in md5 cryption
     if (count($errors) ==0){
         $password = md5($password_first);
-        $query = "INSERT INTO users (username, user_type, password) 
-  			  VALUES('$username', '$user_type', '$password')";
+        $query = "INSERT INTO users (username, name, user_type, password) 
+  			  VALUES('$username', '$name', '$user_type', '$password')";
         $result = $db->query($query);
         if(!$result){
             echo "Wrong in the script";
@@ -98,6 +103,47 @@ function login() {
     }
 
 }
+if (isset($_POST["Usern"])){
+    deleteuser();
+}
+//delete user
+function deleteuser()
+{
+    global $db, $username, $errors;
+    //get values
+    $username = e($_POST['username']);
+
+
+    // make sure form is filled properly
+    if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    $user_check = "SELECT username FROM users WHERE username='$username'";
+    $result = $db->query($user_check);
+    $user = mysqli_fetch_assoc($result);
+    if (!$user) {
+        array_push($errors, "Not a user");
+    } else {
+        if (count($errors) == 0) {
+
+            $query = "DELETE FROM users WHERE username='$username'";
+            $result = $db->query($query);
+
+            if (!$result) {
+                echo "couldnt delete user";
+
+
+
+
+            } else {
+                echo "user deleted";
+            }
+
+
+        }
+
+    }
+}
 //making sure you only enter if logged in
 function isLoggedIN()
 {
@@ -117,6 +163,7 @@ function admin()
         return false;
     }
 }
+//fadder cant login to some place
 function fadder(){
       if (isset($_SESSION['user']) && $_SESSION['user']['user_type'] == 'fadder'){
         return true;
@@ -124,6 +171,7 @@ function fadder(){
         return false;
     }
 }
+//HR and admin can get to hr
 function HR(){
     if (isset($_SESSION['user']) && $_SESSION['user']['user_type'] == 'admin'){
         return true;
@@ -135,36 +183,144 @@ function HR(){
     }
 }
 //logout function on logout.php
-function logO(){
-   if (isset($_SESSION['user'])){
-       return true;
-   }
+function logO()
+{
+    if (isset($_SESSION['user'])) {
+        return true;
+    }
     return true;
 }
-
-//get userid
-function getUserByID($id){
-    global $db;
-    $query = "SELECT * FROM users WHERE id=" . $id;
-    $result = $db->query($query);
-    $user = mysqli_fetch_assoc($result);
-    return $user;
+//if use the edit form go to editpass function
+if (isset($_POST["edit"])){
+    editpass();
 }
-// escape string
-function e($val){
-    global $db;
-    return mysqli_real_escape_string($db, trim($val));
+//if use typeedit form got to edittype function
+if(isset($_POST["type_edit"])){
+    edittype();
 }
-function display_error()
+if (isset($_GET['search'])){
+    searchuser();
+}
+//edit a usertype
+function edittype()
 {
-    global $errors;
+    global $db, $username, $errors;
+    $username = e($_POST['username']);
+    $user_type = e($_POST['user_type']);
+    $user_check = "SELECT username FROM users WHERE username= '$username'";
+    $result = $db->query($user_check);
+    $user = mysqli_fetch_assoc($result);
+    if (!$user) {
+        echo "not a user";
+        array_push($errors, "Not a user");
+    } else {
 
-    if (count($errors) > 0) {
-        echo '<div class="error">';
-        foreach ($errors as $error) {
-            echo $error . '<br>';
+            if (count($errors) == 0){
+                $query = "UPDATE users SET user_type = '$user_type' WHERE username='$username'";
+                $result = $db->query($query);
+                if(!$result){
+                    echo "wrong in the script";
+                }else{
+                    echo "user type edited successful";
+                }
+            }
+
+    }
+
+    }
+
+
+//edit the password of a user
+function editpass()
+{
+    global $db, $username, $errors;
+
+        $username = e($_POST['username']);
+        $original_password =  e($_POST['original_password']);
+        $new_password =  e($_POST['new_password']);
+        $original_password = md5($original_password);
+    $user_check = "SELECT username FROM users WHERE username= '$username'";
+    $result = $db->query($user_check);
+    $user = mysqli_fetch_assoc($result);
+    if (!$user) {
+        array_push($errors, "Not a user");
+    } else {
+        $pass_check = "SELECT password from users WHERE password= '$original_password'";
+        $result = $db->query($pass_check);
+        $pass = mysqli_fetch_assoc($result);
+        if (!$pass) {
+        array_push($errors, "not a valid password");
+        }else {
+
+            //edit password adnd cryptate the password in md5 cryption
+            if (count($errors) == 0) {
+                $password = md5($new_password);
+                $query = "UPDATE users SET password = '$password' WHERE username='$username'";
+                $result = $db->query($query);
+                if (!$result) {
+                    echo "Wrong in the script";
+                } else {
+                    echo "password edit succesful";
+                }
+            }
         }
-        echo '</div>';
+
+    }
+
+
+}
+//search for a user
+function searchuser()
+{
+    global $db, $username, $errors;
+    $username = e($_GET['username']);
+    $user_check = "SELECT username FROM users WHERE username= '$username'";
+    $result = $db->query($user_check);
+    $user = mysqli_fetch_assoc($result);
+    if (!$user) {
+        array_push($errors, "Not a user");
+    } else {
+        if (count($errors) == 0) {
+            $query = "SELECT *, NULL AS password FROM users WHERE username= '$username'";
+            $result = $db->query($query);
+            if (!$result) {
+                echo "viewing failed";
+            } else {
+                while ($row = $result->fetch_object()) {
+                    echo "<li>"."ID number ". $row->ID . " user " . $row->username . " is named " . $row->name . " has the role " . $row->user_type . ". " . $row->password . "</li>";
+                }
+            }
+        }
     }
 }
+//get userid
+    function getUserByID($id)
+    {
+        global $db;
+        $query = "SELECT * FROM users WHERE id=" . $id;
+        $result = $db->query($query);
+        $user = mysqli_fetch_assoc($result);
+        return $user;
+    }
+
+// escape string
+    function e($val)
+    {
+        global $db;
+        return mysqli_real_escape_string($db, trim($val));
+    }
+
+    function display_error()
+    {
+        global $errors;
+
+        if (count($errors) > 0) {
+            echo '<div class="error">';
+            foreach ($errors as $error) {
+                echo $error . '<br>';
+            }
+            echo '</div>';
+        }
+    }
+
 

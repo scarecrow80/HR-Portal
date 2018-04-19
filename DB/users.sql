6cb93e0085b7a94dec0efedd20c2d5ebca82ad82
Scarecrow80 <hvidsten@hotmail.com>
2018-04-19 12:59:50 +0200

    oppdatert

--- DB/users.sql
+++ DB/users.sql
@@ -42,13 +42,13 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`idUsers`, `firstname`, `lastname`, `username`, `usertype`, `password`) VALUES
-(2, 'Dag-Roar', 'Aks', 'Dag-R', 'admin', '440aa7896891b295d14b9e3830a87377'),
-(4, 'Karl', 'Hansen', 'Karli', 'mentor', '3a6af38bf9f736b5a40439f2fa128a16'),
-(5, 'Sarah', 'Fox', 'Sarahi', 'HR', '3fbb450fc9906929513d784cef8845ee'),
-(6, 'Kari', 'Nilsen', 'Kari2', 'leader', '531d61dbedbd283c20e937857eccfcd2'),
-(7, 'Marianne', 'Lauritsen', 'Mario', 'admin', '46c977d719dd1852817c1d4a7db8322d'),
-(8, 'Oda', 'Selmer', 'Odali', 'mentor', '061c93d63ce2f0806a912e9e7f024ed9'),
-(9, 'Sigurd', 'Falkenberg', 'Falk', 'mentor', 'ee3d98bb65407eb8ed17a535b36c2706');
+  (2, 'Dag-Roar', 'Aks', 'Dag-R', 'admin', '440aa7896891b295d14b9e3830a87377'),
+  (4, 'Karl', 'Hansen', 'Karli', 'mentor', '3a6af38bf9f736b5a40439f2fa128a16'),
+  (5, 'Sarah', 'Fox', 'Sarahi', 'HR', '3fbb450fc9906929513d784cef8845ee'),
+  (6, 'Kari', 'Nilsen', 'Kari2', 'leader', '531d61dbedbd283c20e937857eccfcd2'),
+  (7, 'Marianne', 'Lauritsen', 'Mario', 'admin', '46c977d719dd1852817c1d4a7db8322d'),
+  (8, 'Oda', 'Selmer', 'Odali', 'mentor', '061c93d63ce2f0806a912e9e7f024ed9'),
+  (9, 'Sigurd', 'Falkenberg', 'Falk', 'mentor', 'ee3d98bb65407eb8ed17a535b36c2706');
 
 --
 -- Indexes for dumped tables
@@ -74,3 +74,4 @@ ALTER TABLE `users`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+

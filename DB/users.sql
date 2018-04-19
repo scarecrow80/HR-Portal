0a2cb2305216c796217220c27ce8f04f290487ea
Scarecrow80 <hvidsten@hotmail.com>
2018-04-19 16:24:51 +0200

    Forandret på users.sql så den bare setter inn i tabell, ikke oppretter

--- DB/users.sql
+++ DB/users.sql
@@ -1,8 +1,8 @@
-INSERT INTO `Users`(`idUsers`, `firstname`, `lastname`, `username`, `usertype`, `password`) VALUES
-  (2, 'Dag-Roar', 'Aks', 'Dag-R', 'admin', '440aa7896891b295d14b9e3830a87377'),
-  (4, 'Karl', 'Hansen', 'Karli', 'mentor', '3a6af38bf9f736b5a40439f2fa128a16'),
-  (5, 'Sarah', 'Fox', 'Sarahi', 'HR', '3fbb450fc9906929513d784cef8845ee'),
-  (6, 'Kari', 'Nilsen', 'Kari2', 'leader', '531d61dbedbd283c20e937857eccfcd2'),
-  (7, 'Marianne', 'Lauritsen', 'Mario', 'admin', '46c977d719dd1852817c1d4a7db8322d'),
-  (8, 'Oda', 'Selmer', 'Odali', 'mentor', '061c93d63ce2f0806a912e9e7f024ed9'),
-  (9, 'Sigurd', 'Falkenberg', 'Falk', 'mentor', 'ee3d98bb65407eb8ed17a535b36c2706');
+INSERT INTO `Users`(`firstname`, `lastname`, `username`, `usertype`, `password`) VALUES
+  ('Dag-Roar', 'Aks', 'Dag-R', 'admin', '440aa7896891b295d14b9e3830a87377'),
+  ('Karl', 'Hansen', 'Karli', 'mentor', '3a6af38bf9f736b5a40439f2fa128a16'),
+  ('Sarah', 'Fox', 'Sarahi', 'HR', '3fbb450fc9906929513d784cef8845ee'),
+  ('Kari', 'Nilsen', 'Kari2', 'leader', '531d61dbedbd283c20e937857eccfcd2'),
+  ('Marianne', 'Lauritsen', 'Mario', 'admin', '46c977d719dd1852817c1d4a7db8322d'),
+  ('Oda', 'Selmer', 'Odali', 'mentor', '061c93d63ce2f0806a912e9e7f024ed9'),
+  ('Sigurd', 'Falkenberg', 'Falk', 'mentor', 'ee3d98bb65407eb8ed17a535b36c2706');

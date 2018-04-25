3c4b314f7a7bdc058db3ad0060a90e5cd2d6eed9
Robin <robinstadveen@gmail.com>
2018-04-25 17:38:01 +0200

    Header og footer i egen fil

---
+++ Elements/Header.php
@@ -0,0 +1,15 @@
+
+<div id="top">
+    <div class="contentWrapper">
+        <a id="logo" href="http://www.hioa.no/"><img width="236" height="auto" alt="Logo - HiOA - Tilbake til forsida HiOA" src="img/hioa-logo-web_697×120_no.png" /></a>
+        <nav>
+            <div id="navcontainer" class="fullsizeBlock">
+
+                <ul id="topMenu">
+                    <li><a href="http://www.hioa.no/eng">EnglishTODO</a></li>
+                </ul>
+
+            </div> <!-- END: navcontainer -->
+        </nav>
+    </div> <!-- contentWrapper -->
+</div><!-- top -->

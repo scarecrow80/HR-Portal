a0240ae5684d9f1c1f50ecd4346c939a2be8c278
Robin <robinstadveen@gmail.com>
2018-04-26 13:55:16 +0200

    Lagt inn ny oversikt

--- hrDepartment.php
+++ hrDepartment.php
@@ -179,37 +179,90 @@ include('../HR-Portal/DBconnections/dbconnection.php');
                                         });
                                     </script>
                                     <section class="section section-events article-toggle" role="region">
-                                        <article class="h-card vcard person-card article-contact" role="article">
-                                            <h3 title="Oversikt over sjekklister" class="toggler-header article-contact-heading">september 2016</h3>
-                                            <div class="toggler-content">
-                                                <p>TODOTODOTODOTODOTODO</p>
-                                            </div>
-                                        </article>
+                                        <?php
+                                        $db = mysqli_connect("student.cs.hioa.no", "s236619", "", "s236619");
+                                        //$db = mysqli_connect("localhost", "root", "", "db_hr_portal");
+                                        if(!$db){
+                                            die("Feil i databasetilkobling:".$db->connect_error);
+                                        }
 
-                                        <article class="h-card vcard person-card article-contact" role="article">
-                                            <h3 title="Oversikt over sjekklister" class="toggler-header article-contact-heading">oktober 2016</h3>
-                                            <div class="toggler-content">
-                                                <p>TODOTODOTODOTODOTODO Overview</p>
-                                                <?php
-                                                if(!$db){
-                                                    die("Feil i databasetilkobling:".$db->connect_error);
-                                                }
-                                                $query = "select * from Checklist ";
-                                                $result = $db->query($query);
-                                                if(!$result){
-                                                    echo "viewing failed";
-                                                }
-                                                else{
-                                                    while ($row = $result->fetch_object()){
-                                                        echo "<li>".$row->idChecklist. " ".$row->checkpointsNO." responsible is ".$row->responsible."</li>";
-                                                    }
-                                                }?>
+                                        $qry =  "SELECT idNewemployee,firstname, lastname FROM Newemployee";
+                                        $res = mysqli_query($db, $qry);
+                                        if(!$res){
+                                            echo "query failed";
+                                        }
 
-                                            </div>
-                                        </article>
+
+                                        while($row = mysqli_fetch_assoc($res)){
+                                            $id_new = $row['idNewemployee'];
+                                            $f_name = $row['firstname'];
+                                            $l_name = $row['lastname'];
+
+
+                                            $article = ' <article class="h-card vcard person-card article-contact" role="article"><h3 title="Oversikt over sjekklister"  class="toggler-header article-contact-heading"> ';
+                                            $article.=$f_name." ".$l_name." ";
+                                            $article.= '</h3><div class="toggler-content"><form action="" method="post"><table><tr><th>Oppgave</th><th>Sjekkboks</th></tr>';
+                                            $qry2 = "SELECT Newemployee_idNewemployee, Checklist_idChecklist, checked FROM Newemployee_has_Checklist WHERE Newemployee_idNewemployee='$id_new'";
+                                            $res2 = mysqli_query($db, $qry2);
+
+                                            if(!$res2){
+                                                echo "RES2 er tom";
+                                                die();
+                                            }
+                                            while($row2 = mysqli_fetch_assoc($res2)){
+                                                $check_id = $row2['Checklist_idChecklist'];
+                                                $checked = $row2['checked'];
+                                                $emp_id = $row2['Newemployee_idNewemployee'];
+
+                                                $qry3 = "SELECT checkpointsNO, idChecklist from Checklist WHERE idChecklist ='$check_id'";
+                                                $res3 =  mysqli_query($db, $qry3);
+                                                $res4 = mysqli_fetch_assoc($res3);
+
+                                                $article.='
+                                             <tr>
+                                             <td>';
+                                                $article.=" ".$res4['checkpointsNO']." ";
+                                                $id_check=$res4['idChecklist'];
+                                                $article.='</td>';
+                                                $article.='<td height="30px" >';
+                                                if($checked == 0){
+                                                    $article.='<input type="checkbox" class="checkbox" name="';
+                                                    $article.=$emp_id;
+                                                    $article.='" value="';
+                                                    $article.=$checked;
+                                                    $article.='" id="';
+                                                    $article.=$check_id;
+                                                    $article.='" onclick="test(this.name, this.id, this.value)"/>';
+
+                                                } else{
+                                                    $article.='<input type="checkbox" class="checkbox" name="empty" checked onclick="postData(this.name, this.value, this.id)" value="';
+
+                                                    $article.=$checked;
+
+                                                    $article.='">';
+
+                                                }
+
+                                                $article.='</td>
+                                            </tr>';
+
+                                            }
+                                            //$article.='<button type="submit">Submit</button>';
+                                            $article.= '</table></form></div></article>';
+                                            echo $article;
+
+                                        }
+                                        ?>
+                                        <p id="test"></p>
+                                        <script>
+                                            var inputElem = document.getElementsByTagName("checkbox");
+
+
+                                        </script>
                                     </section>
 
                                 </div>
+
 
                                 <div id="search" class="page tilsatt" style="display:none">
                                     <p>TODOTODOTODOTODOTODO search for user</p>

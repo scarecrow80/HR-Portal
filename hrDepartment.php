6c905124383aa68371e78f7a1814dc1d7c788e26
John André Seem <s305366@stud.hioa.no>
2018-05-02 14:02:09 +0200

    HR login

--- hrDepartment.php
+++ hrDepartment.php
@@ -1,7 +1,7 @@
 <?php
 include('../HR-Portal/DBconnections/dbconnection.php');
-if (!leader()){
-    $_SESSION['msg'] = "You have to log in as leader";
+if (!HR()){
+    $_SESSION['msg'] = "You have to log in as one from hr the department ";
     session_destroy();
     unset($_SESSION['user']);
     header('location: ../HR-Portal/index.php');

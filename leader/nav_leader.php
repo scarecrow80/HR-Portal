93c46a9f75a89999aade4f0f56f9cf33896e7bf8
Robin <robinstadveen@gmail.com>
2018-05-04 13:46:05 +0200

    Nye filene for leader, overviews og php fil for meta heads..

---
+++ leader/nav_leader.php
@@ -0,0 +1,27 @@
+<div id="navcontainer" class="fullsizeBlock">
+
+    <ul id="topMenu">
+        <li><a href="http://www.hioa.no/eng">EnglishTODO</a></li>
+    </ul>
+
+    <ul id="Nav">
+        <li class="active active " id="tzju_" role="presentation">
+            <a class="list" href="leader_overview.php" role="menuitem" title="Oversikt"> <span class="nav-item-label"> Oversikt </span> </a>
+        </li>
+        <li class=" " id="lami_" role="presentation">
+            <a class="list" href="leader_owntasks.php" role="menuitem" title="Mine oppgaver"> <span class="nav-item-label"> Mine oppgaver</span> </a>
+        </li>
+        <li class=" " id="ahej_" role="presentation">
+            <a class="list" href="leader_createlist.php"" id="" role="menuitem" title="Opprett sjekkliste"> <span class="nav-item-label"> Opprett sjekkliste </span> </a>
+        </li>
+        <li class=" " id="fyzs_" role="presentation">
+            <a class="list" href="leader_assign.php" id="" role="menuitem" title="Tildel fadder"> <span class="nav-item-label"> Tildel fadder </span> </a>
+        </li>
+        <li class=" " id="fyzs_" role="presentation">
+            <a class="list" onclick="window.location='../HR-Portal/logout.php'" id="" role="menuitem" title="Logg ut"> <span class="nav-item-label"> Logg ut</span> </a>
+        </li>
+
+    </ul>
+
+
+</div>
---
+++ leader/nav_leader_mobile.php
@@ -0,0 +1,14 @@
+<ul>
+    <li class="main-menu">
+        <a class="list" href="leader_overview.php" role="menuitem" title="Oversikt"> <span class="nav-item-label"> Oversikt </span> </a>
+    </li>
+    <li class="main-menu">
+        <a class="list" href="leader_owntasks.php" role="menuitem" title="Mine oppgaver"> <span class="nav-item-label"> Mine oppgaver</span> </a>
+    </li>
+    <li class="main-menu">
+        <a class="list" id="" href="leader_createlist.php" role="menuitem" title="Opprett sjekkliste"> <span class="nav-item-label"> Opprett sjekkliste </span> </a>
+    </li>
+    <li class="main-menu">
+        <a class="list" id="" href="leader_assign.php" role="menuitem" title="Tildel fadder"> <span class="nav-item-label"> Tildel fadder </span> </a>
+    </li>
+</ul>

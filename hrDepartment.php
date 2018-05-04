6723a9ba2951a78df73ceb54c1e72dc6da5946ba
Robin <robinstadveen@gmail.com>
2018-05-04 13:58:10 +0200

    Oppdatert for total oversikt og mine oppgaver - fikset session. Snart utdatert

--- hrDepartment.php
+++ hrDepartment.php
@@ -1,6 +1,6 @@
 <?php
 include('../HR-Portal/DBconnections/dbconnection.php');
-if (!leader()){
+if (!HR()){
     $_SESSION['msg'] = "You have to log in as leader";
     session_destroy();
     unset($_SESSION['user']);
@@ -85,6 +85,9 @@ if (!leader()){
                     <a class="list" onclick="openPage('overview')" role="menuitem" title="Oversikt"> <span class="nav-item-label"> Oversikt </span> </a>
                 </li>
                 <li class="main-menu">
+                    <a class="list" onclick="openPage('mytasks')" role="menuitem" title="Mine oppgaver"> <span class="nav-item-label"> Mine oppgaver </span> </a>
+                </li>
+                <li class="main-menu">
                     <a class="list" id="" onclick="openPage('search')" role="menuitem" title="Søk"> <span class="nav-item-label"> Søk </span> </a>
                 </li>
             </ul>
@@ -103,8 +106,11 @@ if (!leader()){
                     </ul>
 
                     <ul id="Nav">
-                        <li class="active active " id="tzju_" role="presentation">
+                        <li class="active active " id="tzju_9" role="presentation">
                             <a class="list" onclick="openPage('overview')" role="menuitem" title="Oversikt"> <span class="nav-item-label"> Oversikt </span> </a>
+                        </li>
+                        <li class="active active " id="asdqr" role="presentation">
+                            <a class="list" onclick="openPage('mytasks')" role="menuitem" title="Mine oppgaver"> <span class="nav-item-label"> Mine oppgaver</span> </a>
                         </li>
                         <li class=" " id="ahej_" role="presentation">
                             <a class="list" onclick="openPage('search')" id="" role="menuitem" title="Søk"> <span class="nav-item-label"> Søk </span> </a>
@@ -180,90 +186,52 @@ if (!leader()){
                                     </script>
                                     <section class="section section-events article-toggle" role="region">
                                         <?php
-                                        $db = mysqli_connect("student.cs.hioa.no", "s236619", "", "s236619");
-                                        //$db = mysqli_connect("localhost", "root", "", "db_hr_portal");
-                                        //include '../HR-Portal/DBconnections/dbconnection.php';
-                                        if(!$db){
-                                            die("Feil i databasetilkobling:".$db->connect_error);
-                                        }
-                                        //$userId = $_SESSION;
-                                        $qry =  "SELECT Newemployee.firstname, Newemployee.lastname, Newemployee.idNewemployee FROM Newemployee INNER JOIN Users_has_Newemployee ON Newemployee.idNewemployee = Users_has_Newemployee.Newemployee_idNewemployee WHERE Users_has_Newemployee.Users_idUsers = 20";
-                                        $res = mysqli_query($db, $qry);
-                                        if(!$res){
-                                            echo "query failed";
-                                        }
-
-
-                                        while($row = mysqli_fetch_assoc($res)){
-                                            $id_new = $row['idNewemployee'];
-                                            $f_name = $row['firstname'];
-                                            $l_name = $row['lastname'];
-
-
-                                            $article = ' <article class="h-card vcard person-card article-contact" role="article"><h3 title="Oversikt over sjekklister"  class="toggler-header article-contact-heading"> ';
-                                            $article.=$f_name." ".$l_name." ";
-                                            $article.= '</h3><div class="toggler-content"><form action="" method="post"><table><tr><th>Oppgave</th><th>Sjekkboks</th></tr>';
-                                            $qry2 = "SELECT Newemployee_idNewemployee, Checklist_idChecklist, checked FROM Newemployee_has_Checklist INNER JOIN Checklist ON idChecklist WHERE Checklist_idChecklist = idChecklist AND responsible = 'HR' AND Newemployee_idNewemployee='$id_new'";
-                                            $res2 = mysqli_query($db, $qry2);
-
-                                            if(!$res2){
-                                                echo "RES2 er tom";
-                                                die();
-                                            }
-                                            while($row2 = mysqli_fetch_assoc($res2)){
-                                                $check_id = $row2['Checklist_idChecklist'];
-                                                $checked = $row2['checked'];
-                                                $emp_id = $row2['Newemployee_idNewemployee'];
-
-                                                $qry3 = "SELECT checkpointsNO, idChecklist from Checklist WHERE idChecklist ='$check_id' AND responsible='HR'";
-                                                $res3 =  mysqli_query($db, $qry3);
-                                                $res4 = mysqli_fetch_assoc($res3);
-
-                                                $article.='
-                                             <tr>
-                                             <td>';
-                                                $article.=" ".$res4['checkpointsNO']." ";
-                                                $id_check=$res4['idChecklist'];
-                                                $article.='</td>';
-                                                $article.='<td height="30px" >';
-                                                if($checked == 0){
-                                                    $article.='<input type="checkbox" class="checkbox" name="';
-                                                    $article.=$emp_id;
-                                                    $article.='" value="';
-                                                    $article.=$checked;
-                                                    $article.='" id="';
-                                                    $article.=$check_id;
-                                                    $article.='" onclick="test(this.name, this.id, this.value)"/>';
-
-                                                } else{
-                                                    $article.='<input type="checkbox" class="checkbox" name="empty" checked onclick="postData(this.name, this.value, this.id)" value="';
-
-                                                    $article.=$checked;
-
-                                                    $article.='">';
-
-                                                }
-
-                                                $article.='</td>
-                                            </tr>';
-
-                                            }
-                                            //$article.='<button type="submit">Submit</button>';
-                                            $article.= '</table></form></div></article>';
-                                            echo $article;
-
-                                        }
+                                        include "Overview_leader_all.php";
                                         ?>
-                                        <p id="test"></p>
+
                                         <script>
-                                            var inputElem = document.getElementsByTagName("checkbox");
+                                         //   var inputElem = document.getElementsByTagName("checkbox");
 
 
                                         </script>
                                     </section>
-
+                                </div>
                                 </div>
 
+                            <div class="section">
+                                <a id="nonav3" class="hiddenTxt" name="nonav3"></a>
+                            <div id="firstGrid">
+                            <div style="flot:left;clear:both;">
+                                <div id="mytasks" class="page tilsatt" style="display:none">
+                                    <p>Mine oppgaver</p>
+                                    <script>
+                                        YUI().use("aui-toggler", function(a) {
+                                            new a.TogglerDelegate({
+                                                animated: true,
+                                                closeAllOnExpand: true,
+                                                container: ".article-toggle",
+                                                content: ".toggler-content",
+                                                expanded: false,
+                                                header: ".toggler-header",
+                                                transition: {
+                                                    duration: 0.2,
+                                                    easing: "cubic-bezier(0, 0.1, 0, 1)"
+                                                }
+                                            })
+                                        });
+                                    </script>
+
+
+                                    <section class="section section-events article-toggle" role="region">
+                                        <?php
+                                        include "Overview_hr_own.php";
+                                        ?>
+
+                                    </section>
+                                </div>
+                                </div>
+                            </div>
+                            </div>
 
                                 <div id="search" class="page tilsatt" style="display:none">
                                     <p>TODOTODOTODOTODOTODO search for user</p>

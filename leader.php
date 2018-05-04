9382300c2a700ab98bdf14fa6c531f44a37d9956
Robin <robinstadveen@gmail.com>
2018-05-04 13:58:55 +0200

    (Utdatert nå)

--- leader.php
+++ leader.php
@@ -60,8 +60,39 @@ if (!leader()){
     <link rel="stylesheet" type="text/css"  href="css/upgrade_rearrange_fixes.css" />
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="http://cdn.alloyui.com/2.5.0/aui/aui-min.js"></script>
     <script>
-        $(document).ready(function() {
+        YUI().use('aui-toggler',
+            function(Y) {
+                new Y.TogglerDelegate({
+                    animated: true,
+                    closeAllOnExpand: true,
+                    container: "#test",
+                    content: ".toggler-content",
+                    expanded: false,
+                    header: ".toggler-header",
+                    transition: {
+                        duration: 0.2,
+                        easing: "cubic-bezier(0, 0.1, 0.1)"
+                    }
+                }).render();
+            });
+    </script>
+    <script>
+
+        function openPage(pageName){
+            var i;
+            var x = document.getElementsByClassName("page");
+            for (i = 0; i < x.length; i++){
+                x[i].style.display = "none";
+            }
+            document.getElementById(pageName).style.display = "block";
+        }
+    </script>
+
+
+    <script>
+    /*    $(document).ready(function() {
 
             $("input").keyup(function() {
                 var name = $("input").val();
@@ -94,7 +125,7 @@ if (!leader()){
 
 
 
-
+*/
 
 
     </script>
@@ -124,6 +155,9 @@ if (!leader()){
                     <a class="list" onclick="openPage('overview')" role="menuitem" title="Oversikt"> <span class="nav-item-label"> Oversikt </span> </a>
                 </li>
                 <li class="main-menu">
+                    <a class="list" onclick="openPage('own_task')" role="menuitem" title="Mine oppgaver"> <span class="nav-item-label"> Mine oppgaver</span> </a>
+                </li>
+                <li class="main-menu">
                     <a class="list" id="" onclick="openPage('createList')" role="menuitem" title="Opprett sjekkliste"> <span class="nav-item-label"> Opprett sjekkliste </span> </a>
                 </li>
                 <li class="main-menu">
@@ -148,25 +182,21 @@ if (!leader()){
                         <li class="active active " id="tzju_" role="presentation">
                             <a class="list" onclick="openPage('overview')" role="menuitem" title="Oversikt"> <span class="nav-item-label"> Oversikt </span> </a>
                         </li>
+                        <li class=" " id="lami_" role="presentation">
+                            <a class="list" onclick="openPage('own_task')" role="menuitem" title="Mine oppgaver"> <span class="nav-item-label"> Mine oppgaver</span> </a>
+                        </li>
                         <li class=" " id="ahej_" role="presentation">
                             <a class="list" onclick="openPage('createList')" id="" role="menuitem" title="Opprett sjekkliste"> <span class="nav-item-label"> Opprett sjekkliste </span> </a>
                         </li>
                         <li class=" " id="fyzs_" role="presentation">
                             <a class="list" onclick="openPage('assignMentor')" id="" role="menuitem" title="Tildel fadder"> <span class="nav-item-label"> Tildel fadder </span> </a>
                         </li>
+                        <li class=" " id="fyzs_" role="presentation">
+                            <a class="list" onclick="window.location='../HR-Portal/logout.php'" id="" role="menuitem" title="Logg ut"> <span class="nav-item-label"> Logg ut</span> </a>
+                        </li>
 
                     </ul>
-                    <script>
 
-                        function openPage(pageName){
-                            var i;
-                            var x = document.getElementsByClassName("page");
-                            for (i = 0; i < x.length; i++){
-                                x[i].style.display = "none";
-                            }
-                            document.getElementById(pageName).style.display = "block";
-                        }
-                    </script>
 
                 </div> <!-- END: navcontainer -->
             </nav>
@@ -195,7 +225,7 @@ if (!leader()){
                 <div id="section">		          <!-- Main area content: START -->
 
                     <div class="section">
-                        <a id="nonav3" class="hiddenTxt" name="nonav3"></a>
+                        <!-- <a id="nonav3" class="hiddenTxt" name="nonav3"></a> Hva gjør denne?-->
 
 
 
@@ -203,115 +233,46 @@ if (!leader()){
                         <div id="firstGrid">		<!-- <div class="venstrekolonne"> -->
                             <!-- </div> --> <!-- END: venstrekolonne -->		<!-- <div class="hoyrekolonne"> -->
                             <!-- </div> --> <!-- END: hoyrekolonne -->
-                            <div style="flot:left;clear:both;">
+                        <div style="flot:left;clear:both;">
 
-                                <script src="http://cdn.alloyui.com/2.5.0/aui/aui-min.js"></script>
+
+                                <div id="test">
                                 <div id="overview" class="page tilsatt">
 
-                                    <script>
-                                        YUI().use("aui-toggler", function(a) {
-                                            new a.TogglerDelegate({
-                                                animated: true,
-                                                closeAllOnExpand: true,
-                                                container: ".article-toggle",
-                                                content: ".toggler-content",
-                                                expanded: false,
-                                                header: ".toggler-header",
-                                                transition: {
-                                                    duration: 0.2,
-                                                    easing: "cubic-bezier(0, 0.1, 0, 1)"
-                                                }
-                                            })
-                                        });
-                                    </script>
+
                                     <section class="section section-events article-toggle" role="region">
                                         <?php
-                                        $db = mysqli_connect("student.cs.hioa.no", "s236619", "", "s236619");
-                                        //$db = mysqli_connect("localhost", "root", "", "db_hr_portal");
-                                        //include '../HR-Portal/DBconnections/dbconnection.php';
-                                        if(!$db){
-                                            die("Feil i databasetilkobling:".$db->connect_error);
-                                        }
-                                        //$userId = $_SESSION;
-                                        $qry =  "SELECT Newemployee.firstname, Newemployee.lastname, Newemployee.idNewemployee FROM Newemployee INNER JOIN Users_has_Newemployee ON Newemployee.idNewemployee = Users_has_Newemployee.Newemployee_idNewemployee WHERE Users_has_Newemployee.Users_idUsers = 19";
-                                        $res = mysqli_query($db, $qry);
-                                        if(!$res){
-                                            echo "query failed";
-                                        }
+                                        include "Overview_leader_all.php";
 
-
-                                        while($row = mysqli_fetch_assoc($res)){
-                                            $id_new = $row['idNewemployee'];
-                                            $f_name = $row['firstname'];
-                                            $l_name = $row['lastname'];
-
-
-                                            $article = ' <article class="h-card vcard person-card article-contact" role="article"><h3 title="Oversikt over sjekklister"  class="toggler-header article-contact-heading"> ';
-                                            $article.=$f_name." ".$l_name." ";
-                                            $article.= '</h3><div class="toggler-content"><form action="" method="post"><table><tr><th>Oppgave</th><th>Sjekkboks</th></tr>';
-                                            $qry2 = "SELECT Newemployee_idNewemployee, Checklist_idChecklist, checked FROM Newemployee_has_Checklist INNER JOIN Checklist ON idChecklist WHERE Checklist_idChecklist = idChecklist AND responsible = 'Leder' AND Newemployee_idNewemployee='$id_new'";
-                                            $res2 = mysqli_query($db, $qry2);
-
-                                            if(!$res2){
-                                                echo "RES2 er tom";
-                                                die();
-                                            }
-                                            while($row2 = mysqli_fetch_assoc($res2)){
-                                                $check_id = $row2['Checklist_idChecklist'];
-                                                $checked = $row2['checked'];
-                                                $emp_id = $row2['Newemployee_idNewemployee'];
-
-                                                $qry3 = "SELECT checkpointsNO, idChecklist from Checklist WHERE idChecklist ='$check_id' AND responsible='Leder'";
-                                                $res3 =  mysqli_query($db, $qry3);
-                                                $res4 = mysqli_fetch_assoc($res3);
-
-                                                $article.='
-                                             <tr>
-                                             <td>';
-                                                $article.=" ".$res4['checkpointsNO']." ";
-                                                $id_check=$res4['idChecklist'];
-                                                $article.='</td>';
-                                                $article.='<td height="30px" >';
-                                                if($checked == 0){
-                                                    $article.='<input type="checkbox" class="checkbox" name="';
-                                                    $article.=$emp_id;
-                                                    $article.='" value="';
-                                                    $article.=$checked;
-                                                    $article.='" id="';
-                                                    $article.=$check_id;
-                                                    $article.='" onclick="test(this.name, this.id, this.value)"/>';
-
-                                                } else{
-                                                    $article.='<input type="checkbox" class="checkbox" name="empty" checked onclick="postData(this.name, this.value, this.id)" value="';
-
-                                                    $article.=$checked;
-
-                                                    $article.='">';
-
-                                                }
-
-                                                $article.='</td>
-                                            </tr>';
-
-                                            }
-                                            //$article.='<button type="submit">Submit</button>';
-                                            $article.= '</table></form></div></article>';
-                                            echo $article;
-
-                                        }
                                         ?>
-                                        <p id="test"></p>
+
                                         <script>
-                                            var inputElem = document.getElementsByTagName("checkbox");
+                                          // var inputElem = document.getElementsByTagName("checkbox");
 
 
                                         </script>
                                     </section>
 
+
+                                </div>
+                    </div>
+
+                                <div id="test">
+                                <div id="own_task" class="page tilsatt" style="flot:left;clear:both;">
+
+
+
+                                    <section class="section section-events article-toggle" role="region">
+
+
+
+                                    </section>
+
+                                </div>
                                 </div>
 
                                 <div id="createList" class="page tilsatt" style="display:none">
-                                    <p>TODOTODOTODOTODOTODO create list</p>
+                                    <p>Create list</p>
                                     <form action="" method="post">
                                         <table>
                                             <tr class="input-group">
@@ -352,7 +313,7 @@ if (!leader()){
                                 </div>
 
                                 <div id="assignMentor" class="page tilsatt" style="display:none">
-                                    <p>TODOTODOTODOTODOTODO assign mentor</p>
+                                    <p>Assign mentor</p>
                                     <form action="" method="post">
                                         <table>
                                             <tr class="input-group">
@@ -387,23 +348,12 @@ if (!leader()){
                                     Logout
                                     </button>
                                 </div>
-                                <script>
-
-                                    function openPage(pageName){
-                                        var i;
-                                        var x = document.getElementsByClassName("page");
-                                        for (i = 0; i < x.length; i++){
-                                            x[i].style.display = "none";
-                                        }
-                                        document.getElementById(pageName).style.display = "block";
-                                    }
-                                </script>
 
                             </div><!-- </div> --> <!-- END: innholdskolonne -->
                         </div> <!-- END: section -->
 
 
-
+                    </div>
                         <!-- Main area content: END -->
                     </div>
                 </div>
@@ -472,7 +422,9 @@ if (!leader()){
                             <ul>
                                 <li>
                                     <a class="imagelink fortilsatte" href="https://www.hioa.no/For-tilsatte">For tilsatte</a>
-                                </li>
+</html>
+
+</li>
                                 <li>
                                     <a href="https://prod.cms.hioa.no/admin" target="_blank" title="Login">eZ Publish</a>
                                 </li>
@@ -489,8 +441,7 @@ if (!leader()){
     <!-- </div> -->
     <!-- Complete page area: END -->
     <!-- Footer script area: START --><!-- Footer script area: END -->
+
     <script type="text/javascript" src="js/all.js" charset="utf-8"></script>
 
-
 </body>
-</html>

116aa6fa3333ab66e9b3dd6fcfc0edd2c0076450
Robin <robinstadveen@gmail.com>
2018-05-09 13:47:18 +0200

    Oppdatert slik at den kan lagre sjekkboks

--- DBconnections/leaderController.php
+++ DBconnections/leaderController.php
@@ -23,7 +23,7 @@ function overviewLeader(){
 
         $article = ' <article class="h-card vcard person-card article-contact" role="article"><h3 title="Oversikt over sjekklister"  class="toggler-header article-contact-heading"> ';
         $article.=$f_name." ".$l_name." ";
-        $article.= '</h3><div class="toggler-content"><form action="" method="post"><table><tr><th>Oppgave</th><th>Sjekkboks</th></tr>';
+        $article.= '</h3><div class="toggler-content"><form action="../../DBconnections/update.php" method="post"><table><tr><th>Oppgave</th><th>Sjekkboks</th></tr>';
         $qry2 = "SELECT Newemployee_idNewemployee, Checklist_idChecklist, checked FROM Newemployee_has_Checklist INNER JOIN Checklist ON idChecklist WHERE Checklist_idChecklist = idChecklist AND responsible = 'Leder' AND Newemployee_idNewemployee='$id_new'";
         $res2 = mysqli_query($db, $qry2);
 
@@ -40,37 +40,27 @@ function overviewLeader(){
             $res3 =  mysqli_query($db, $qry3);
             $res4 = mysqli_fetch_assoc($res3);
 
-            $article.='
-                                             <tr>
-                                             <td>';
+            $article.='<tr><td>';
             $article.=" ".$res4['checkpointsNO']." ";
-            $id_check=$res4['idChecklist'];
+
             $article.='</td>';
             $article.='<td height="30px" >';
             if($checked == 0){
-                $article.='<input type="checkbox" class="checkbox" name="';
-                $article.=$emp_id;
+                $article.='<input type="checkbox" class="checkbox" name="formList[]"';
                 $article.='" value="';
-                $article.=$checked;
+                $article.=$checked." ".$emp_id." " .$check_id;
                 $article.='" id="';
                 $article.=$check_id;
-                $article.='" onclick="test(this.name, this.id, this.value)"/>';
+                $article.='" />';
 
             } else{
-                $article.='<input type="checkbox" class="checkbox" name="empty" checked onclick="postData(this.name, this.value, this.id)" value="';
-
-                $article.=$checked;
-
-                $article.='">';
-
-            }
-
-            $article.='</td>
-                                            </tr>';
+                $article.='<input type="checkbox" class="checkbox" checked value="1"';
+                $article.='" name="formList[]">';
+             }
+             $article.='</td></tr>';
 
         }
-        //$article.='<button type="submit">Submit</button>';
-        $article.= '</table></form></div></article>';
+        $article.= '</table><br/><button type="submit" class="btn btn-primary">Oppdater</button></form></div></article>';
         echo $article;
 
     }

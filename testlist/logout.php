13babba94aeaa2152d006712b1402caa2b87d9fe
scarecrow80 <scarecrow80@users.noreply.github.com>
2018-05-22 21:22:39 +0200

    Delete logout.php

--- testlist/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('../testlist/dbconnection.php');
-if (logO()){
-    $_SESSION['msg'] = "Logged out";
-    session_destroy();
-    unset($_SESSION['user']);
-    header( 'location: ../testlist/homepage.php');
-
-}
